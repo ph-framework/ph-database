abfcbb3f1833d6cc47eca704ea3424a9c3f870cf
YuriDeV15 <yurilucasmaturana@gmail.com>
2024-04-09 14:22:37 -0300

    feat: Add return in insert, replace and update functions

--- src/server/modules/replace.ts
+++ src/server/modules/replace.ts
@@ -2,76 +2,80 @@
 import { config, connection, locale } from "@server/core"
 import { Specifications } from "@server/protocols"
 import { Pool } from "mysql2/typings/mysql/lib/Pool"
+import { formatResponse } from "./formatResponse"
 
 // Replace
 const replaceExport = async <T = Specifications>(
     table: string,
     specifications: Partial<T>
-): Promise<void> => {
+): Promise<Array<T>> => {
     try {
         const before = new Date().getMilliseconds()
         const invokingResource = GetInvokingResource()
 
-        let hasTimedOut = false
+        return new Promise((resolve, reject) => {
+            let hasTimedOut = false
 
-        const timeoutId = setTimeout(() => {
-            hasTimedOut = true
-            console.error(
-                locale.logs.functionTimeouted
-                    .replace("[FUNCTION]", "replace")
-                    .replace("[TABLE]", table)
-                    .replace("[RESOURCE]", invokingResource)
-                    .replace("[TIME]", config.functionTimeout.toString())
-            )
-        }, config.functionTimeout)
-
-        if (config.debug)
-            console.log(
-                locale.debug.called
-                    .replace("[FUNCTION]", "replace")
-                    .replace("[TABLE]", table)
-                    .replace("[RESOURCE]", invokingResource)
-            )
-
-        if (!hasTimedOut) {
-            const keys = Object.keys(specifications).join(", ")
-            const values = Object.values(specifications)
-                .map((value) =>
-                    typeof value === "string" ? `"${value}"` : value
+            setTimeout(() => {
+                hasTimedOut = true
+                reject(
+                    locale.logs.functionTimeouted
+                        .replace("[FUNCTION]", "replace")
+                        .replace("[TABLE]", table)
+                        .replace("[RESOURCE]", invokingResource)
+                        .replace("[TIME]", config.functionTimeout.toString())
                 )
-                .join(", ")
+            }, config.functionTimeout)
 
-            const queryString = `REPLACE INTO ${table} (${keys}) VALUES (${values})`
+            if (config.debug)
+                console.log(
+                    locale.debug.called
+                        .replace("[FUNCTION]", "replace")
+                        .replace("[TABLE]", table)
+                        .replace("[RESOURCE]", invokingResource)
+                )
 
-            const execute = (connection as Pool).promise().execute(queryString)
+            if (!hasTimedOut) {
+                const keys = Object.keys(specifications).join(", ")
+                const values = Object.values(specifications)
+                    .map((value) =>
+                        typeof value === "string" ? `"${value}"` : value
+                    )
+                    .join(", ")
 
-            return execute
-                .then((): void => {
-                    const after = new Date().getMilliseconds()
-                    const responseTime = after - before
+                const queryString = `REPLACE INTO ${table} (${keys}) VALUES (${values})`
 
-                    if (config.debug)
-                        console.log(
-                            locale.debug.response
+                const query = (connection as Pool).promise().query(queryString)
+
+                query
+                    .then((rows): void => {
+                        const response = formatResponse(rows) as Array<T>
+
+                        const after = new Date().getMilliseconds()
+                        const responseTime = after - before
+
+                        if (config.debug)
+                            console.log(
+                                locale.debug.response
+                                    .replace("[FUNCTION]", "replace")
+                                    .replace("[TIME]", responseTime.toString())
+                                    .replace("[QUERY]", queryString)
+                            )
+
+                        resolve(response)
+                    })
+                    .catch(() =>
+                        reject(
+                            locale.logs.functionError
                                 .replace("[FUNCTION]", "replace")
-                                .replace("[TIME]", responseTime.toString())
                                 .replace("[QUERY]", queryString)
                         )
-
-                    clearTimeout(timeoutId)
-                })
-                .catch(() => {
-                    console.error(
-                        locale.logs.functionError
-                            .replace("[FUNCTION]", "replace")
-                            .replace("[QUERY]", queryString)
                     )
-
-                    clearTimeout(timeoutId)
-                })
-        }
+            }
+        })
     } catch (e) {
         console.error(e)
+        return []
     }
 }
 
